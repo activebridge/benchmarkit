8b7511e89128f73eaf9227ee626137bf1f2cfd42
olegsobchuk <oleg.sobchuk@gmail.com>
2015-12-31 16:47:42 +0200

    Add comment's style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.save
-    redirect_to battle_path(@battle)
+    # redirect_to battle_path(@battle)
   end
 
   private
@@ -15,8 +15,8 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params[:comment].merge!(head_battle_id: @battle.id)
+    params[:comment].merge!(head_battle_id: @battle.id, user_id: current_user.id)
     params[:comment][:comment_battle_attributes].merge!(before: @battle.before)
-    params.require(:comment).permit(:text, :head_battle_id, comment_battle_attributes: [:before, :after, :result])
+    params.require(:comment).permit(:text, :head_battle_id, :user_id, comment_battle_attributes: [:before, :after, :result])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,18 @@
 class Comment < ActiveRecord::Base
+  validate :text_or_battle?
   belongs_to :head_battle, class_name: 'Battle'
   belongs_to :comment_battle, class_name: 'Battle'
   belongs_to :user
 
   accepts_nested_attributes_for :comment_battle, allow_destroy: true
+
+  private
+
+  def text_or_battle?
+    if comment_battle&.invalid?
+      self.comment_battle = nil
+      self.valid?
+      errors.add(:comment, 'Comment should have Battle, Text or both.') if text.empty?
+    end
+  end
 end
